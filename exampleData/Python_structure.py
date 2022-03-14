11b9eb698ca5b59cbc1bdabc14f26bda89666a71
BrodyCooke <89492471+BrodyCooke@users.noreply.github.com>
2022-03-14 13:58:25 -0400

    ConvertsToJson

---
+++ exampleData/Python_structure.py
@@ -0,0 +1,42 @@
+import json
+match = []
+for i in range(1,3):
+    map_name = "Oasis"
+    map_score = [0,0]
+    map_type = "Control"
+
+    map_dic = {
+        "map": map_name,
+        "map_score": '',
+        "map_type": '',
+    }
+
+    name = "Golex"
+    role = "main_tank"
+    avg_time_to_ult = 88.375
+    avg_time_ult_held = 19.857
+    final_stats = {}
+    stats_per_minute = {}
+    ult_timings = []
+    heroes_played = {}
+
+    for i in range(1,13):
+        player = {
+            "name": name,
+            "role": role,
+            "avg_time_to_ult": avg_time_to_ult,
+            "avg_time_ult_held": avg_time_ult_held,
+            "final_stats": final_stats,
+            "stats_per_minute": stats_per_minute,
+            "ult_timings": ult_timings,
+            "heroes_played": heroes_played
+        }
+        player_number = "player" + str(i)
+        map_dic[player_number] = player
+
+    print(map_dic)
+    match.append(map_dic)
+
+print(match)
+json_match = json.dumps(match)
+print(json_match)
