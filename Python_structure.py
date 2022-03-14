9a599d6eef0998c37803dea6078e3d2caafdc5a6
BrodyCooke <89492471+BrodyCooke@users.noreply.github.com>
2022-03-14 17:36:52 -0400

    Added Comments

--- Python_structure.py
+++ Python_structure.py
@@ -1,11 +1,14 @@
 import json
 
+# opens a test .json file and writes the contents to that file
+# only works if your folder is named PurdueStat for now
 def writeJson(contents):
     f = open("PurdueStat/demofile.json", "w")
     f.write(contents)
     f.close()
     return
 
+# all of these definitions output test data and need to be filled in later
 def getMapName():
     return "Oasis"
 
@@ -39,6 +42,7 @@ def getUltTimings():
 def getHeroesPlayed():
     return {}
 
+# creates a dictionary of the functions specified above
 functionDict = {
     "getMapName": getMapName(),
     "getMapScore": getMapScore(),
@@ -52,20 +56,24 @@ functionDict = {
     "getUltTimings": getUltTimings(),
     "getHeroesPlayed": getHeroesPlayed()
 }
-
+# this line only runs when this script is ran as main
 if __name__ == '__main__':
     match = []
+    # currently loops twice, would need to loop for as many files as we have
+    # this loop adds entries into the match list, allowing more than 1 map be to represented
     for i in range(1,3):
         '''    map_name = "Oasis"
         map_score = [0,0]
         map_type = "Control"'''
 
+        # creates the outermoust dictionary that is for each map
         map_dic = {
             "map": functionDict["getMapName"],
             "map_score": functionDict["getMapScore"],
             "map_type": functionDict["getMapType"],
         }
 
+        # this loop creates each entry for the 12 player
         for i in range(1,13):
             player = {
                 "name": functionDict["getName"],
@@ -78,12 +86,17 @@ if __name__ == '__main__':
                 "heroes_played": functionDict["getHeroesPlayed"]
             }
             player_number = "player" + str(i)
+            # updates the map dictionary with the current player of the loop
             map_dic[player_number] = player
 
-        #print(map_dic)
+        # this line adds the map to the match list
         match.append(map_dic)
 
-    #print(match)
+    # converts match to a json format
     json_match = json.dumps(match, indent=4)
+    # the output printed to terminal
     print(json_match)
+
+    # when uncommented this line will update the demofile.json
+            # I commeneted it so that if your file structure was different it would not error
     #writeJson((json_match))
