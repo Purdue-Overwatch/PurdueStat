8dd6873b8b6c3827dbc05faf14581dee68ed57da
ZachBeacon <89481556+ZachBeacon@users.noreply.github.com>
2022-03-14 17:18:21 -0500

    Stats Update
    Updated Test.py to include statspermin and final stats don't have time right now to finish updating

--- test.py
+++ test.py
@@ -1,1 +1,179 @@
-print("test")
+def getAllDamageDealt():
+    return 6942066.6
+
+def getBarrierDamage():
+    return 137.7
+
+def getCooldown1():
+    return 0.0
+
+def getCooldown2():
+    return 7.03
+
+def getDamageBlocked():
+    return 10730.0
+
+def getDamageTaken():
+    return 17221.91
+
+def getDeaths():
+    return 69
+
+def getEliminations():
+    return 53
+
+def getEnviroDeaths():
+    return 2
+
+def getEnviroKills():
+    return 1
+
+def getFinalBlows():
+    return 13
+
+def getHealingDealt():
+    return 0.0
+
+def getHealingReceived():
+    return 0.0
+
+def getHeroDamageDealt():
+    return 9416.48
+
+def getObjectiveKills():
+    return 12
+
+def getSoloKills():
+    return 2
+
+def getUltimateCharge():
+    return 100.0
+
+def getUltimatesEarned():
+    return 5
+
+def getUltimatesUsed():
+    return 2
+
+def getAllDamageTen():
+    return 1123.652099031216
+
+def getBarrierDamageTen():
+    return 515.48
+
+def getCooldown1Ten():
+    return 0.0
+
+def getCooldown2Ten():
+    return 0.454
+
+def getDamageBlockedTen():
+    return 693.003
+
+def getDamageTakenTen():
+    return 1112.2869
+
+def getDeathsTen():
+    return 0.645855
+
+def getEliminationsTen():
+    return 1.94
+
+def getEnviroDeathsTen():
+    return 0.06458
+
+def getEnviroKillsTen():
+    return 0.0
+
+def getFinalBlowsTen():
+    return 0.8396
+
+def getHealingDealtTen():
+    return 0.0
+
+def getHealingReceivedTen():
+    return 0.0
+
+def getHeroDamageDealtTen():
+    return 608.1687
+
+def getObjectiveKillsTen():
+    return 0.775
+
+def getSoloKillsTen():
+    return 1000
+
+def getUltimateChargeTen():
+    return 6.458
+
+def getUltimatesEarnedTen():
+    return 0.322927
+
+def getUltimatesUsedTen():
+    return 0.12917
+
+finalstatDfunc = {
+    "getAllDamageDealt": getAllDamageDealt(),
+    "getBarrierDamage": getBarrierDamage(),
+    "getCooldown1": getCooldown1(),
+    "getCooldown2": getCooldown2(),
+    "getDamageBlocked": getDamageBlocked(),
+    "getDamageTaken": getDamageTaken(),
+    "getDeaths": getDeaths(),
+    "getEliminations": getEliminations(),
+    "getEnviroDeaths": getEnviroDeaths(),
+    "getEnviroKills": getEnviroKills(),
+    "getFinalBlows": getFinalBlows(),
+    "getHealingDealt": getHealingDealt(),
+    "getHealingReceived": getHealingReceived(),
+    "getHeroDamageDealt": getHeroDamageDealt(),
+    "getObjectiveKills": getObjectiveKills(),
+    "getSoloKills": getSoloKills(),
+    "getUltimateCharge": getUltimateCharge(),
+    "getUltimatesEarned": getUltimatesEarned(),
+    "getUltimatesUsed": getUltimatesUsed()
+}
+def getFinalStats():
+    return {
+        "all_damage_dealt": finalstatDfunc["getAllDamageDealt"],
+        "barrier_damage_dealt": finalstatDfunc["getBarrierDamage"],
+        "cooldown1": finalstatDfunc["getCooldown1"],
+        "cooldown2": finalstatDfunc["getCooldown2"],
+        "damage_blocked": finalstatDfunc["getDamageBlocked"],
+        "damage_taken": finalstatDfunc["getDamageTaken"],
+        "deaths": finalstatDfunc["getDeaths"],
+        "eliminations": finalstatDfunc["getEliminations"],
+        "environmental_deaths": finalstatDfunc["getEnviroDeaths"],
+        "environmental_kills": finalstatDfunc["getEnviroKills"],
+        "final_blows": finalstatDfunc["getFinalBlows"],
+        "healing_dealt": finalstatDfunc["getHealingDealt"],
+        "healing_received": finalstatDfunc["getHealingReceived"],
+        "hero_damage_dealt": finalstatDfunc["getHeroDamageDealt"],
+        "objective_kills": finalstatDfunc["getObjectiveKills"],
+        "solo_kills": finalstatDfunc["getSoloKills"],
+        "ultimate_charge": finalstatDfunc["getUltimateCharge"],
+        "ultimates_earned": finalstatDfunc["getUltimatesEarned"],
+        "ultimates_used": finalstatDfunc["getUltimatesUsed"]
+
+    }
+statperminDFunc = {
+    "getAllDamageTen": getAllDamageTen(),
+    "getBarrierDamageTen": getBarrierDamageTen(),
+    "getCooldown1Ten": getCooldown1Ten(),
+    "getCooldown2Ten": getCooldown2Ten(),
+    "getDamageBlockedTen": getDamageBlockedTen(),
+    "getDamageTakenTen": getDamageTakenTen(),
+    "getDeathsTen": getDeathsTen(),
+    "getEliminationsTen": getEliminationsTen(),
+    "getEnviroDeathsTen": getEnviroDeathsTen(),
+    "getEnviroKillsTen": getEnviroKillsTen(),
+    "getFinalBlowsTen": getFinalBlowsTen(),
+    "getHealingDealtTen": getHealingDealtTen(),
+    "getHealingReceivedTen": getHealingReceivedTen(),
+    "getHeroDamageDealtTen": getHeroDamageDealtTen(),
+    "getObjectiveKillsTen": getObjectiveKillsTen(),
+    "getSoloKillsTen": getSoloKillsTen(),
+    "getUltimateChargeTen": getUltimateChargeTen(),
+    "getUltimatesEarnedTen": getUltimatesEarnedTen(),
+    "getUltimatesUsedTen": getUltimatesUsedTen()
+}
