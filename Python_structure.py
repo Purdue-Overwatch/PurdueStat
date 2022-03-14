814740b77ddaea417df9f8d5c2dd7b1d33c3f408
BrodyCooke <89492471+BrodyCooke@users.noreply.github.com>
2022-03-14 15:28:15 -0400

    getter functions done and implemented

--- Python_structure.py
+++ Python_structure.py
@@ -1,5 +1,14 @@
 import json
 
+def getMapName():
+    return "Oasis"
+
+def getMapScore():
+    return [0,0]
+
+def getMapType():
+    return "Control"
+
 def getName():
     return "Golex"
 
@@ -12,56 +21,62 @@ def getTimeToUlt():
 def getTimeUltHeld():
     return 19.857
 
-def finalStats():
+def getFinalStats():
+    return {}
+
+def getStatsPerMinute():
+    return {}
+
+def getUltTimings():
+    return []
+
+def getHeroesPlayed():
     return {}
 
 functionDict = {
-    "getName": getName,
-    "getRole": getRole,
-    "getTimeToUlt": getTimeToUlt,
-    "getTimeUltHeld": getTimeUltHeld,
-    "finalStats":finalStats,
+    "getMapName": getMapName(),
+    "getMapScore": getMapScore(),
+    "getMapType": getMapType(),
+    "getName": getName(),
+    "getRole": getRole(),
+    "getTimeToUlt": getTimeToUlt(),
+    "getTimeUltHeld": getTimeUltHeld(),
+    "getFinalStats": getFinalStats(),
+    "getStatsPerMin": getStatsPerMinute(),
+    "getUltTimings": getUltTimings(),
+    "getHeroesPlayed": getHeroesPlayed()
 }
 
 if __name__ == '__main__':
     match = []
     for i in range(1,3):
-        map_name = "Oasis"
+        '''    map_name = "Oasis"
         map_score = [0,0]
-        map_type = "Control"
+        map_type = "Control"'''
 
         map_dic = {
-            "map": map_name,
-            "map_score": '',
-            "map_type": '',
+            "map": functionDict["getMapName"],
+            "map_score": functionDict["getMapScore"],
+            "map_type": functionDict["getMapType"],
         }
-
-        name = "Golex"
-        role = "main_tank"
-        avg_time_to_ult = 88.375
-        avg_time_ult_held = 19.857
-        final_stats = {}
-        stats_per_minute = {}
-        ult_timings = []
-        heroes_played = {}
 
         for i in range(1,13):
             player = {
-                "name": name,
-                "role": role,
-                "avg_time_to_ult": avg_time_to_ult,
-                "avg_time_ult_held": avg_time_ult_held,
-                "final_stats": final_stats,
-                "stats_per_minute": stats_per_minute,
-                "ult_timings": ult_timings,
-                "heroes_played": heroes_played
+                "name": functionDict["getName"],
+                "role": functionDict["getRole"],
+                "avg_time_to_ult": functionDict["getTimeToUlt"],
+                "avg_time_ult_held": functionDict["getTimeUltHeld"],
+                "final_stats": functionDict["getFinalStats"],
+                "stats_per_minute": functionDict["getStatsPerMin"],
+                "ult_timings": functionDict["getUltTimings"],
+                "heroes_played": functionDict["getHeroesPlayed"]
             }
             player_number = "player" + str(i)
             map_dic[player_number] = player
 
-        print(map_dic)
+        #print(map_dic)
         match.append(map_dic)
 
-    print(match)
+    #print(match)
     json_match = json.dumps(match)
     print(json_match)
