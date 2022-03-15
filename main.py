fe4d279dcac4349e252a4b4b45a375e69cfda94b
BrodyCooke <89492471+BrodyCooke@users.noreply.github.com>
2022-03-15 11:02:02 -0400

    fix main.py formatting according to pycharm, fixed demofile.json output after testing last night

--- main.py
+++ main.py
@@ -12,15 +12,17 @@ minStatDict = functions.getStatsPerMin()
 
 genFunctDict = functions.getGenFunctions()
 
+
 # ignore this for now
 def main(filepath: str) -> int:
     pass
-    
+
+
 if __name__ == '__main__':
     match = []
     # currently loops twice, would need to loop for as many files as we have
     # this loop adds entries into the match list, allowing more than 1 map be to represented
-    for i in range(1): # ,2):
+    for i in range(0,2):
         # creates the outermoust dictionary that is for each map
         map_dic = {
             "map": genFunctDict["getMapName"],
@@ -28,9 +30,12 @@ if __name__ == '__main__':
             "map_type": genFunctDict["getMapType"],
         }
 
-        # this loop creates each entry for the 12 player
+        # these lines set default values for the player variables so that they can't be undefined
+        player_number = "NAN"
+        player = {}
 
-        for i in range(1, 2):
+        # this loop creates each entry for the 12 player
+        for i in range(1, 13):
             player = {
                 "name": genFunctDict["getName"],
                 "role": genFunctDict["getRole"],
@@ -43,19 +48,17 @@ if __name__ == '__main__':
             }
             player_number = "player" + str(i)
         # updates the map dictionary with the current player of the loop
-        map_dic[player_number] = player
+            map_dic[player_number] = player
 
         # this line adds the map to the match list
         match.append(map_dic)
-        print(map_dic)
+        # print(map_dic)
 
     # converts match to a json format
     json_match = json.dumps(match, indent=4)
     # the output printed to terminal
-    print(json_match)
+    # print(json_match)
 
-    # when uncommented this line will update the demofile.json
-            # I commeneted it so that if your file structure was different it would not error
     f = open("demofile.json", "w")
     f.write(json_match)
     f.close()
