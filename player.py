9ead30b5922050d48112b81f4b592652cd406c9d
Techno <82276766+TechnoWizzy@users.noreply.github.com>
2022-05-12 23:33:44 -0400

    Update player

--- player.py
+++ player.py
@@ -1,13 +1,25 @@
-'''
-Creating Player class so we can have multiple player objects exist at the same time to query player statistics against each other in the future.
-'''
+"""
+A Player Class
+Updated: 5/12/2022 - TechnoWizzy
+"""
+
+
 class Player:
-    '''
+    """
     Player object
-    '''
+    """
 
-    def get_role():
-        return "role"
-
-    def set_role(self, role):
+    def __init__(self, name, hero, role):
         self._role = role
+        self._hero = hero
+        self._name = name
+        self._final_blows = 0
+        self._deaths = 0
+        self._hero_dmg = 0
+        self._barrier_dmg = 0
+        self._heals = 0
+        self._dmg = 0
+        self._ults_earned = 0
+        self._ults_used = 0
+        self._avg_ult_time = 0
+        self._avg_ult_held = 0
