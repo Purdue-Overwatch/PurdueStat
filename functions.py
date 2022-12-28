11c3b0fe8dbc22651de3a6f14ded6ee48e47d13b
josh park <wljoshpark@gmail.com>
2022-12-27 19:21:54 -0500

    fix local repo -josh

--- functions.py
+++ functions.py
@@ -3,133 +3,52 @@ Defines getter functions and organizes them into dictionaries to be imported int
 
 Defines utility function readLogFile for reading in an Overwatch log file and seperating it into an event file, a CSV file, and an
 info file.
-Defines utility function CSVToArray for converting an input CSV to a python 3 dimensional list
 '''
 
-import re
-import csv
 import os
+import pymongo
 
-path = os.path.dirname(__file__)
-
-# This function takes in a logfile filename and creates 3 temp files that can be found in the testingTempfiles folder
-# takes an input of a filepath and has no outputs
-
-"""NOT COMPLETED"""
-"""needs tested for not KOTH maps"""
-def readLogfile(filename):
-
-    # opens each file in the mode needed
-    logfile = open(filename, 'r')
-    csvfile = open(f"{path}\\testingTempfiles/tempCSV.txt", 'a')
-    eventfile = open(f"{path}\\testingTempfiles/tempEvents.txt", 'a')
-    mapInfofile = open(f"{path}\\testingTempfiles/tempMapInfo.txt", 'w')
-
-    # removes the previous contents of these files since they are opened in append
-    csvfile.truncate(0)
-    eventfile.truncate(0)
-
-    # if a word in this list is in the line it will move that line to the tempevents file
-    listOfEventTriggers = ["FinalBlow", "Resurrected", "DuplicatingStart", "DuplicatingEnd"]
-    listOfMapInfoTriggers = ["False", "True"]
-    # splits the first 2 lines that contain the map info into thier own file
-    for i in range(0,2):
-        mapLines = logfile.readline()
-        mapInfofile.write(str(mapLines))
-
-    # splits the events from the rest of the info so that a csv format file is left
-    for line in logfile:
-        if any(trigger in line for trigger in listOfEventTriggers):
-            eventfile.write(line)
-        if any(trigger in line for trigger in listOfMapInfoTriggers) | (
-                re.match("\[\d+:\d+:\d+] [0-9]+\.[0-9]+,(([0-9]+\.[0-9]+)|\d+),(([0-9]+\.[0-9]+)|\d+)\n", line) is not None):
-            mapInfofile.write(line)
-        else:
-            csvfile.write(line)
-
-    logfile.close()
-    csvfile.close()
-    eventfile.close()
-    mapInfofile.close()
-    pass
+address = os.getenv('PURDUESTAT_MONGO_ADDRESS')
+client = pymongo.MongoClient(address)
+db = client['game_server']
+collection = db['games']
+game_db = collection.find_one({'_id': 'dbecea12163e'}) # take in id
 
 
-    ''' 
-    convert the input CSV file to a list that holds each row of a matrix
-    array is a list of lists where each entry in array is a line of the input file and that line is broken into a list 
-    using comas as separators
-    array[0][0] is the first line of the input file and the first separated string
-    array[0][1] is the first line and the second separated string, and so on
-    '''
-def CSVToArray(filename) -> list:
-    with open(filename) as CSVfile:
-        file_read = csv.reader(CSVfile)
-        array = list(file_read)
-    return array
+'''Constants'''
+TANKS = ['Reinhardt', 'Winston', 'Orisa', 'Wrecking Ball', 'Roadhog', 'Zarya', 'Sigma', 'D.Va', ]
+DPS = ['Echo', 'Pharah', 'Doomfist', 'Junkrat', 'Mei', 'Sombra', 'Torbjorn', 'Genji', 'Hanzo', 'Symmetra', 'Reaper', 'Soldier: 76', 'Tracer', 'Bastion', 'Ashe', 'Cassidy', 'Widowmaker', 'Sojourn', ]
+SUPPORTS = ['L\u00c3\u00bacio', 'Brigitte', 'Mercy', 'Moira', 'Zenyatta', 'Baptiste', 'Ana', 'Kiriko', ]
+
+# Begin basic functions
+def get_map_name(game) -> str:
+    return game_db['_map_name']
+
+# Required?
+# def getMapScore():
+#     return [0, 0]
 
 
-# start gen info functions
-# completed
-def getMapName(filename) -> str:
-    map_array = CSVToArray(filename)
-    return map_array[0][0][11:]
+def get_team(name) -> str:
+    for i in range(6):
+        if name is game_db['_team_one']['_players'][i]['_name']:
+            return 'team1'
+    return 'team2'
 
+def define_role(heroes) -> str:
+    if heroes in TANKS:
+        return 'Tank'
+    if heroes in DPS:
+        return 'Dps'
+    if heroes in SUPPORTS:
+        return 'Support'
 
-def getMapScore():
-    return [0, 0]
-
-
-# completed
-def getMapType(filename) -> str:
-    map_name = getMapName(filename)
-    if map_name in ["Busan", "Ilios", "Lijiang Tower", "Nepal", "Oasis"]:
-        return "Control"
-    if map_name in ["Hanamura", "Horizon Lunar Colony", "Paris", "Temple of Anubis", "Volskaya Industries"]:
-        return "Assault"
-    if map_name in ["Dorado", "Havana", "Junkertown", "Rialto", "Route 66", "Watchpoint Gibraltar"]:
-        return "Escort"
-    if map_name in ["Blizzard World", "Eichenwalde", "Hollywood", "King's Row", "Numbani"]:
-        return "Hybrid"
-    else:
-        print("NAN")
-
-
-# completed
-def getTeam(player_name) -> str:
-    map_info = CSVToArray("testingTempfiles/tempMapInfo.txt")
-    # team1 = map_info[0][1]
-    # team2 = map_info[0][2]
-    teamlist = map_info[1][:]
-    teamlist[0] = teamlist[0][11:]
-
-    if player_name in teamlist[0:6]:
-        return "team1"
-    elif player_name in teamlist[6:12]:
-        return "team2"
-    else:
-        return "notFound"
-
-
-# completed
-def defineRole(heroes) -> str:
-    if heroes in ["Reinhardt", "Orisa", "Winston","D.Va", "Roadhog", "Sigma", "Wrecking Ball", "Zarya"]:
-        return "Tank"
-    if heroes in ["Ashe", "Bastion", "Cassidy", "Reaper", "Soldier: 76", "Sombra", "Tracer", "Widowmaker","Doomfist", "Echo", "Genji", "Hanzo", "Junkrat", "Mei", "Pharah", "Symmetra", "Torbjorn"]:
-        return "Dps"
-    if heroes in ["L\u00c3\u00bacio", "Zenyatta", "Mercy", "Brigitte","Ana", "Baptiste", "Moira"]:
-        return "Support"
-
-
-# completed
 def assignRoles(players, heroes):
-    tanklist = ["Reinhardt", "Winston", "Orisa", "Wrecking Ball", "Roadhog", "Zarya", "Sigma", "D.Va"]
-    dpslist = ["Echo", "Pharah", "Doomfist", "Junkrat", "Mei", "Sombra", "Torbjorn", "Genji", "Hanzo", "Symmetra", "Reaper", "Soldier: 76", "Tracer", "Bastion", "Ashe", "Cassidy", "Widowmaker"]
-    suplist = ["L\u00c3\u00bacio", "Brigitte", "Mercy", "Moira", "Zenyatta", "Baptiste", "Ana"]
     hero1 = heroes[0]
     hero2 = heroes[1]
     output = []
-    if defineRole(heroes[0]) == "Tank":
-        for hero in tanklist:
+    if define_role(heroes[0]) == 'Tank':
+        for hero in TANKS:
             if hero1 == hero:
                 output.append(players[0])
                 output.append(players[1])
@@ -138,8 +57,8 @@ def assignRoles(players, heroes):
                 output.append(players[1])
                 output.append(players[0])
                 break
-    elif defineRole(heroes[0]) == "Dps":
-        for hero in dpslist:
+    elif define_role(heroes[0]) == 'Dps':
+        for hero in DPS:
             if hero1 == hero:
                 output.append(players[0])
                 output.append(players[1])
@@ -148,8 +67,8 @@ def assignRoles(players, heroes):
                 output.append(players[1])
                 output.append(players[0])
                 break
-    elif defineRole(heroes[0]) == "Support":
-        for hero in suplist:
+    elif define_role(heroes[0]) == 'Support':
+        for hero in SUPPORTS:
             if hero1 == hero:
                 output.append(players[0])
                 output.append(players[1])
@@ -161,7 +80,6 @@ def assignRoles(players, heroes):
     return output
 
 
-# completed
 def makePlayerDict(array):
     names = []
     heroes = []
@@ -175,11 +93,11 @@ def makePlayerDict(array):
         names.append(info[0])
         heroes.append(info[1])
     for i in range(0, 12):
-        team = getTeam(names[i])
-        if team == "team1":
+        team = get_team(names[i])
+        if team == 'team1':
             team1players.append(names[i])
             team1heroes.append(heroes[i])
-        elif team == "team2":
+        elif team == 'team2':
             team2players.append(names[i])
             team2heroes.append(heroes[i])
         else:
@@ -192,13 +110,13 @@ def makePlayerDict(array):
         supportplayers = []
         supportheroes = []
         for i in range(0,6):
-            if defineRole(heroes[i]) == "Tank":
+            if define_role(heroes[i]) == 'Tank':
                 tankplayers.append(team[i])
                 tankheroes.append(heroes[i])
-            elif defineRole(heroes[i]) == "Dps":
+            elif define_role(heroes[i]) == 'Dps':
                 dpsplayers.append(team[i])
                 dpsheroes.append(heroes[i])
-            elif defineRole(heroes[i]) == "Support":
+            elif define_role(heroes[i]) == 'Support':
                 supportplayers.append(team[i])
                 supportheroes.append(heroes[i])
             else:
@@ -215,32 +133,30 @@ def makePlayerDict(array):
     return ordered_names
 
 
-# completed
-"""
-Iterate through players from array, when the player names match up return the role they played
-"""
+# Iterate through players from array, when the player names match up return the role they played
+
 def getRole(player_name, array) -> str:
     count = 0
     for player in array:
         if player == player_name:
             if count % 6 == 0:
-                return "main_tank"
+                return 'main_tank'
             elif count % 6 == 1:
-                return "off_tank"
+                return 'off_tank'
             elif count % 6 == 2:
-                return "hitscan_dps"
+                return 'hitscan_dps'
             elif count % 6 == 3:
-                return "flex_dps"
+                return 'flex_dps'
             elif count % 6 == 4:
-                return "main_support"
+                return 'main_support'
             elif count % 6 == 5:
-                return "flex_support"
+                return 'flex_support'
         count += 1
 
 
     for j in range(0, 12):
         if array[j][1] == player_name:
-            return defineRole(array[j][2])
+            return define_role(array[j][2])
     return 'Error'
 
 
@@ -254,7 +170,6 @@ def getUltTimings(player_name, array) -> list:
         [[125, 160], [234, 277], [390, -1]]
     ]
 
-# PARK DO THIS U DUMB FUCK
 def getTimeToUlt():
     return 88.375
 
@@ -263,36 +178,8 @@ def getTimeUltHeld():
     return 19.857
 
 
-def getHeroesPlayed(name, array)->dict:
-    time = 0
-    hero = 'NA'
-    heroes_played = {}
-    herolist = []
-    herotimes = []
-    for line in array:
-
-        if line[1:3][0] == name:
-            temp = hero
-            hero = line[1:3][1]
-            prevhero = temp
-
-            if (hero != prevhero) & (prevhero != 'NA'):
-                herolist.append(prevhero)
-                herotimes.append(time)
-                time = 0
-            time += 1
-    herolist.append(hero)
-    herotimes.append(time)
-    heroes_played["heroes"] = herolist
-    for i in range(len(herolist)):
-        heroes_played[herolist[i]] = herotimes[i]
-
-    return heroes_played
-
-
 # start final stat functions
 
-# completed
 def getFinalEntries(array) -> list:
     length = len(array)
     final_entries = []
@@ -311,97 +198,97 @@ def getFinalInfo(input_name, array, statnum) -> float:
     return None
 
 
-# completed
+
 def getAllDamageDealt(input_name, array) -> float:
     return float(getBarrierDamage(input_name, array)) + float(getHeroDamageDealt(input_name, array))
 
 
-# completed
+
 def getBarrierDamage(input_name, array) -> float:
     return getFinalInfo(input_name, array, 5)
 
 
-# completed
+
 def getCooldown1(input_name, array) -> float:
     return getFinalInfo(input_name, array, 25)
 
 
-# completed
+
 def getCooldown2(input_name, array) -> float:
     return getFinalInfo(input_name, array, 26)
 
 
-# completed
+
 def getDamageBlocked(input_name, array) -> float:
     return getFinalInfo(input_name, array, 6)
 
 
-# completed
+
 def getDamageTaken(input_name, array) -> float:
     return getFinalInfo(input_name, array, 7)
 
 
-# completed
+
 def getDeaths(input_name, array) -> float:
     return getFinalInfo(input_name, array, 8)
 
 
-# completed
+
 def getEliminations(input_name, array) -> float:
     return getFinalInfo(input_name, array, 9)
 
 
-# completed
+
 def getEnviroDeaths(input_name, array) -> float:
     return getFinalInfo(input_name, array, 11)
 
 
-# completed
+
 def getEnviroKills(input_name, array) -> float:
     return getFinalInfo(input_name, array, 12)
 
 
-# completed
+
 def getFinalBlows(input_name, array) -> float:
     return getFinalInfo(input_name, array, 10)
 
 
-# completed
+
 def getHealingDealt(input_name, array) -> float:
     return getFinalInfo(input_name, array, 13)
 
 
-# completed
+
 def getHealingReceived(input_name, array) -> float:
     return getFinalInfo(input_name, array, 18)
 
 
-# completed
+
 def getHeroDamageDealt(input_name, array) -> float:
     return getFinalInfo(input_name, array, 4)
 
 
-# completed
+
 def getObjectiveKills(input_name, array) -> float:
     return getFinalInfo(input_name, array, 14)
 
 
-# completed
+
 def getSoloKills(input_name, array) -> float:
     return getFinalInfo(input_name, array, 15)
 
 
-# completed
+
 def getUltimateCharge(input_name, array) -> float:
     return getFinalInfo(input_name, array, 19)
 
 
-# completed
+
 def getUltimatesEarned(input_name, array) -> float:
     return getFinalInfo(input_name, array, 16)
 
 
-# completed
+
 def getUltimatesUsed(input_name, array) -> float:
     return getFinalInfo(input_name, array, 17)
 
@@ -421,47 +308,47 @@ def convertMin(stat, array) -> float:
 
 def getFinalStats(name, array) -> dict:
     return {
-        "all_damage_dealt": getAllDamageDealt(name, array),
-        "barrier_damage_dealt": getBarrierDamage(name, array),
-        "cooldown1": getCooldown1(name, array),
-        "cooldown2": getCooldown2(name, array),
-        "damage_blocked": getDamageBlocked(name, array),
-        "damage_taken": getDamageTaken(name, array),
-        "deaths": getDeaths(name, array),
-        "eliminations": getEliminations(name, array),
-        "environmental_deaths": getEnviroDeaths(name, array),
-        "environmental_kills": getEnviroKills(name, array),
-        "final_blows": getFinalBlows(name, array),
-        "healing_dealt": getHealingDealt(name, array),
-        "healing_received": getHealingReceived(name, array),
-        "hero_damage_dealt": getHeroDamageDealt(name, array),
-        "objective_kills": getObjectiveKills(name, array),
-        "solo_kills": getSoloKills(name, array),
-        "ultimate_charge": getUltimateCharge(name, array),
-        "ultimates_earned": getUltimatesEarned(name, array),
-        "ultimates_used": getUltimatesUsed(name, array),
+        'all_damage_dealt': getAllDamageDealt(name, array),
+        'barrier_damage_dealt': getBarrierDamage(name, array),
+        'cooldown1': getCooldown1(name, array),
+        'cooldown2': getCooldown2(name, array),
+        'damage_blocked': getDamageBlocked(name, array),
+        'damage_taken': getDamageTaken(name, array),
+        'deaths': getDeaths(name, array),
+        'eliminations': getEliminations(name, array),
+        'environmental_deaths': getEnviroDeaths(name, array),
+        'environmental_kills': getEnviroKills(name, array),
+        'final_blows': getFinalBlows(name, array),
+        'healing_dealt': getHealingDealt(name, array),
+        'healing_received': getHealingReceived(name, array),
+        'hero_damage_dealt': getHeroDamageDealt(name, array),
+        'objective_kills': getObjectiveKills(name, array),
+        'solo_kills': getSoloKills(name, array),
+        'ultimate_charge': getUltimateCharge(name, array),
+        'ultimates_earned': getUltimatesEarned(name, array),
+        'ultimates_used': getUltimatesUsed(name, array),
     }
 
 
 def getStatsPerMin(name, array) -> dict:
     return {
-        "all_damage_dealt": convertMin(getAllDamageDealt(name, array), array),
-        "barrier_damage_dealt": convertMin(getBarrierDamage(name, array), array),
-        "cooldown1": convertMin(getCooldown1(name, array), array),
-        "cooldown2": convertMin(getCooldown2(name, array), array),
-        "damage_blocked": convertMin(getDamageBlocked(name, array), array),
-        "damage_taken": convertMin(getDamageTaken(name, array), array),
-        "deaths": convertMin(getDeaths(name, array), array),
-        "eliminations": convertMin(getEliminations(name, array), array),
-        "environmental_deaths": convertMin(getEnviroDeaths(name, array), array),
-        "environmental_kills": convertMin(getEnviroKills(name, array), array),
-        "final_blows": convertMin(getFinalBlows(name, array), array),
-        "healing_dealt": convertMin(getHealingDealt(name, array), array),
-        "healing_received": convertMin(getHealingReceived(name, array), array),
-        "hero_damage_dealt": convertMin(getHeroDamageDealt(name, array), array),
-        "objective_kills": convertMin(getObjectiveKills(name, array), array),
-        "solo_kills": convertMin(getSoloKills(name, array), array),
-        "ultimate_charge": convertMin(getUltimateCharge(name, array), array),
-        "ultimates_earned": convertMin(getUltimatesEarned(name, array), array),
-        "ultimates_used": convertMin(getUltimatesUsed(name, array), array),
+        'all_damage_dealt': convertMin(getAllDamageDealt(name, array), array),
+        'barrier_damage_dealt': convertMin(getBarrierDamage(name, array), array),
+        'cooldown1': convertMin(getCooldown1(name, array), array),
+        'cooldown2': convertMin(getCooldown2(name, array), array),
+        'damage_blocked': convertMin(getDamageBlocked(name, array), array),
+        'damage_taken': convertMin(getDamageTaken(name, array), array),
+        'deaths': convertMin(getDeaths(name, array), array),
+        'eliminations': convertMin(getEliminations(name, array), array),
+        'environmental_deaths': convertMin(getEnviroDeaths(name, array), array),
+        'environmental_kills': convertMin(getEnviroKills(name, array), array),
+        'final_blows': convertMin(getFinalBlows(name, array), array),
+        'healing_dealt': convertMin(getHealingDealt(name, array), array),
+        'healing_received': convertMin(getHealingReceived(name, array), array),
+        'hero_damage_dealt': convertMin(getHeroDamageDealt(name, array), array),
+        'objective_kills': convertMin(getObjectiveKills(name, array), array),
+        'solo_kills': convertMin(getSoloKills(name, array), array),
+        'ultimate_charge': convertMin(getUltimateCharge(name, array), array),
+        'ultimates_earned': convertMin(getUltimatesEarned(name, array), array),
+        'ultimates_used': convertMin(getUltimatesUsed(name, array), array),
     }
